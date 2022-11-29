b613a38f124a4f8668fae3420c363437f5bc339c
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-29 16:38:10 +0100

    Adding select input

--- functions.php
+++ functions.php
@@ -4,4 +4,6 @@ $elements = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
 
 if (isset($_GET["length"])) {
     $generatedPassword = substr(str_shuffle($elements), 0, $_GET["length"]);
+} elseif (!isset($_GET["length"])) {
+    $generatedPassword = substr(str_shuffle($elements), 0, 0);
 }
